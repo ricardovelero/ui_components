37c10120927b3f3ff46797514a6baf0ac65ba567
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-03-23 10:40:18 +0100

    Primera version del generador de ui:component

---
+++ lib/ui_components.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ui_components/version"
+
+module UiComponents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ui_components/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module UiComponents
+  VERSION = "0.1.0"
+end
